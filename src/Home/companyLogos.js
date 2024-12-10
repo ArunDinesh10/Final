804d220fed151868198f9bd57920528ccaccbd74
arundinesh1011@gmail.com <arundinesh1011@gmail.com>
2024-12-10 12:33:06 -0500

    update

---
+++ src/Home/companyLogos.js
@@ -0,0 +1,9 @@
+const companyLogos = [
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/640px-Google_2015_logo.svg.png',
+    'https://logos-world.net/wp-content/uploads/2020/05/Facebook-Logo-2019.png',
+    'https://a0.awsstatic.com/libra-css/images/logos/aws_logo_smile_1200x630.png',
+    'https://images.ctfassets.net/y2ske730sjqp/1aONibCke6niZhgPxuiilC/2c401b05a07288746ddf3bd3943fbc76/BrandAssets_Logos_01-Wordmark.jpg?w=940',
+  ];
+  
+  export default companyLogos;
+  
